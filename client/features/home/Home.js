7a3d5f6ef40ff8600a483a368895d53235c35f41
Nicole Montes <nikolemontes@gmail.com>
2023-03-22 18:05:32 -0400

    re-worked homePage based on figma

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,19 +1,47 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
+import Navbar from "../navbar/Navbar"
 
 
-/**
- * COMPONENT
- */
-const Home = (props) => {
-  const username = useSelector((state) => state.auth.me.username);
+const Home = () => {
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logoutAndRedirectHome = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
 
   return (
-    <div>
-    <Link to="/groups" className='group-container'>Group</Link>
-    </div>
+    <div className="home-container">
 
+      <div className="logo">
+      </div>
+      <nav className="home-links"> 
+      {/* if its logged in */}
+        {isLoggedIn ? (
+          <div className="home-items">
+            <Link to="/home">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <button
+              type="button"
+              onClick={logoutAndRedirectHome}
+              className="logout"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (   
+          <div className="home-login">
+          <img src="./images/landing-logo.png" alt="Description of image" className="landing-logo"/>
+          <button onClick={() => navigate('/login')} id='login-button'>Login</button>
+          <button onClick={() => navigate('/signup')} id='signup-button'>Sign Up</button>
+        </div>
+        )}
+      </nav>
+    </div>
   );
 };
 
