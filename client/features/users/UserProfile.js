04dcf0215fb17b8310b5501656679d07c55cb60f
budr <thayerjam@gmail.com>
2023-04-03 06:40:31 -0700

    more css!

--- client/features/users/UserProfile.js
+++ client/features/users/UserProfile.js
@@ -1,38 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
+import Profile from "../profile/Profile";
 
 const UserProfile = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div class="bodySizedContainer centeredFlexColumn">
-        <div class="centeredContainer centeredFlexColumn">
-          <h1>Profile</h1>
-          <div id="profileContainer">
-            <div class="profileField">
-              <span>Name</span>
-              <input value="john"></input>
-            </div>
-            <div class="profileField">
-              <span>Email</span>
-              <input value="john"></input>
-            </div>
-            <div class="profileField">
-              <span>Gender</span>
-              <input value="john"></input>
-            </div>
-            <div class="profileField">
-              <span>Age</span>
-              <input value="john"></input>
-            </div>
-            <div class="profileField">
-              <span>Profile Picture</span>
-              <input value="john"></input>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Profile></Profile>
     </div>
   );
 };
