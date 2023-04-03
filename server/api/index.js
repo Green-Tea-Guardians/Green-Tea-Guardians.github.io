239d988d152e6a7a5b58a2ba891b5eefd85bb511
Nicole Montes <112655353+Nikolemar@users.noreply.github.com>
2023-04-03 11:25:26 -0400

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 module.exports = router;
 
-router.use('/users', require('./users')); // Mount the '/users' sub-router
-router.use('/groups', require('./groups')); // Mount the '/groups' sub-router
+router.use('/users', require('./users')); 
+router.use('/groups', require('./groups'));
 
 
 
 // Default error handling middleware
 router.use((req, res, next) => {
-  const error = new Error('Not Found'); // Create a new Error object with a 'Not Found' message
+  const error = new Error('Not Found'); 
   error.status = 404; // Set the error status to 404 (Not Found)
   next(error); // Pass the error to the error handler middleware
 });
