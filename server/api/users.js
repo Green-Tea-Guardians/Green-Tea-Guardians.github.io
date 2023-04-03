9dac3458daafa9696b8658c0d71d97618097aa54
Nicole Montes <112655353+Nikolemar@users.noreply.github.com>
2023-04-03 11:26:01 -0400

    Update users.js

--- server/api/users.js
+++ server/api/users.js
@@ -5,23 +5,23 @@ module.exports = router
 // GET all users
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({ // Retrieve all users from the database
-      attributes: ['id', 'username'] // Include only the 'id' and 'username' fields in the returned data
+    const users = await User.findAll({ 
+      attributes: ['id', 'username'] 
     });
-    res.json(users); // Return the users as JSON
+    res.json(users);
   } catch (err) {
-    next(err); // Pass any errors to the error handler middleware
+    next(err); 
   }
 });
 
 // GET a single user by username
 router.get('/:username', async (req, res, next) => {
   try {
-    const user = await User.findOne({ // Retrieve a user with the given username from the database
+    const user = await User.findOne({ 
       where: {
-        username: req.params.username, // Find the user by their username, which is passed as a URL parameter
+        username: req.params.username, 
       },
-      attributes: ['id', 'username'], // Include only the 'id' field in the returned data
+      attributes: ['id', 'username'], 
     });
     res.json(user); // Return the user as JSON
   } catch (err) {
@@ -32,11 +32,11 @@ router.get('/:username', async (req, res, next) => {
 // GET a single user by id
 router.get('/id/:id', async (req, res, next) => {
   try {
-    const user = await User.findOne({ // Retrieve a user with the given id from the database
+    const user = await User.findOne({ 
       where: {
         id: req.params.id, // Find the user by their id, which is passed as a URL parameter
       },
-      attributes: ['id', 'username'], // Include only the 'id' and 'username' fields in the returned data
+      attributes: ['id', 'username'], 
     });
     res.json(user); // Return the user as JSON
   } catch (err) {
